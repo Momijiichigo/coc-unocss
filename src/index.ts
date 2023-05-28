6a64cfb0f2fe6037a73d41b1d74e84c94a7eb531
Daichi <46286940+Momijiichigo@users.noreply.github.com>
2023-05-28 02:20:29 -0500

    customizable filetype

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,15 @@ interface CocUnocssConfig {
   enable: boolean
 }
 
+
+function dedup<T>(arr1: T[], arr2: T[]): T[] {
+  const set = new Set(arr1)
+  for (const item of arr2) {
+    set.add(item)
+  }
+  return [...set]
+}
+
 export async function activate(context: ExtensionContext): Promise<void> {
   const config = workspace.getConfiguration().get('unocss') as CocUnocssConfig
 
@@ -31,8 +40,15 @@ export async function activate(context: ExtensionContext): Promise<void> {
     args: ['--node-ipc'],
   }
 
+  const filetypes: string[] = (() => {
+    const ft = workspace.getConfiguration('unocss').get('filetypes')
+    if (Array.isArray(ft)) return ft
+    if (typeof ft === 'string') return [ft]
+    throw new Error('unocss.filetypes must be string[] or string')
+  })()
+
   const clientOptions: LanguageClientOptions = {
-    documentSelector: [
+    documentSelector: dedup(filetypes, [
       'vue',
       'html',
       'svelte',
@@ -40,7 +56,7 @@ export async function activate(context: ExtensionContext): Promise<void> {
       'javascriptreact',
       'typescript',
       'typescriptreact',
-    ],
+    ]),
   }
 
   const client = new LanguageClient(
